140768b0732770fa7556d910409c8e4228a0310a
ana rita <anaritadores@gmail.com>
2025-06-08 17:22:13 +0100

    add fprime and rev_wstr

--- level3/ft_atoi_base.c
+++ level3/ft_atoi_base.c
@@ -5,28 +5,23 @@ int	ft_atoi_base(char *str, int base)
 	int	result = 0;
 	int	val;
 
-	// Ignorar espaços em branco
-	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+	while(str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
-	// Sinal
 	if (str[i] == '+' || str[i] == '-')
 	{
-		if (str[i] == '-')
-			sign = -1;
+		if(str[i] == '-')
+			sign *= -1;
 		i++;
 	}
-	// Processar apenas dígitos válidos para a base
-	while (
-		(str[i] >= '0' && str[i] <= '9' && (str[i] - '0' < base)) ||
-		(str[i] >= 'a' && str[i] <= 'f' && (str[i] - 'a' + 10 < base)) ||
-		(str[i] >= 'A' && str[i] <= 'F' && (str[i] - 'A' + 10 < base))
-	)
+	while ((str[i] >= '0' && str[i] <= '9' && (str[i] - 48 < base)) 
+		|| ((str[i] >= 'a' && str[i] <= 'f') && (str[i] - 'a' + 10 < base))
+		|| ((str[i] >= 'A' && str[i] <= 'F') && (str[i] - 'A' + 10 < base)))
 	{
-		if (str[i] >= '0' && str[i] <= '9')
-			val = str[i] - '0';
+		if(str[i] >= '0' && str[i] <= '9')
+			val = str[i] - 48;
 		else if (str[i] >= 'a' && str[i] <= 'f')
-			val = str[i] - 'a' + 10;
-		else // (str[i] >= 'A' && str[i] <= 'F')
+			val = str[i] - 'a' + 10 ;
+		else
 			val = str[i] - 'A' + 10;
 		result = result * base + val;
 		i++;
